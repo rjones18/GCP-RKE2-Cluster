02c160843c26ab9142e5d44d841159ffbe77eb05
Reginald Jones <reginaldjones@Reginalds-MBP.attlocal.net>
2024-03-01 13:31:59 -0600

    Update

--- kubernetes_setup_scripts/node1-script.sh
+++ kubernetes_setup_scripts/node1-script.sh
@@ -10,7 +10,7 @@ sudo mkdir -p /etc/rancher/rke2/
 sudo touch /etc/rancher/rke2/config.yaml
 sudo cat <<"EOF" > /etc/rancher/rke2/config.yaml
 server: https://10.128.0.22:9345
-token: #replace with token
+token: # Put Token Here
 EOF
 sudo systemctl enable rke2-agent.service
 sudo systemctl start rke2-agent.service
